63ee6e6078ff21fd131a6884de1a30f5904ad594
Derek QQ <dereklu76@qq.com>
2024-10-19 12:40:15 +0800

    add some debug information

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ app_path = os.path.join(TEST_DIR, "app")
 # Compile the program
 print("Building...")
 try:
-    ret = subprocess.run(["gcc", code_path, "-o", app_path, "&& ls"],
+    ret = subprocess.run(["gcc", code_path, "-o", app_path],
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             timeout=COMPILER_TIMEOUT)
@@ -35,7 +35,7 @@ if ret.returncode != 0:
 # Run the compiled program
 print("Running...")
 try:
-    ret = subprocess.run([app_path], 
+    ret = subprocess.run(["ls &&", app_path], 
                         stdout=subprocess.PIPE,
                         timeout=RUN_TIMEOUT)
 except Exception as e:
